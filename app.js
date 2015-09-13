c00b7dc52bb3c71c5c3a038466a0fe6a1495c6cb
Philipp Keil <philippkeil95@gmail.com>
2015-09-14 00:02:06 +0200

    added angular.js

--- app.js
+++ app.js
@@ -13,14 +13,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //routing
-app.use(function (req, res, next) {
-  //for all requests print a message
-  console.log('new request:', Date.now());
-  next();
-});
-
 app.get('/', routes.index);
-app.get('/test', routes.test);
 
-console.log('TEST');
 app.listen(61111); //my port on uberspace is 61111
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,4 @@
 exports.index = function (req, res) {
-  res.render('index', {title: 'Test SITE'});
+  res.render('index', {title: 'Philipp Keil'});
   console.log('New GET request on /');
-};
-
-exports.test = function (req, res) {
-  res.sendFile(path.join(__dirname, '/images/test-image.png'));
-  console.log('sending file');
 };
